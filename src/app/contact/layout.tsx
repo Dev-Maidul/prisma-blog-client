8ea28bbc3e590b4396e7567407735d4efa3da6ae
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-01-12 22:51:32 +0800

    Mastering Layout

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ContactLayout({ children }: { children: React.ReactNode}) {
+  return (
+    <div>
+      
+      This is contact layout
+      {children}
+    </div>
+  );
+}

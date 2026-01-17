aae62400fff9f98543ed33e689c6bd6ddddcdb92
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-01-18 01:46:31 +0800

    add login and register page

--- src/app/(commonLayout)/register/page.tsx
+++ src/app/(commonLayout)/register/page.tsx
@@ -1,10 +1,11 @@
-import { LoginForm } from "@/components/authentication/login-form"
+import { RegisterForm } from "@/components/authentication/register-form";
+
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <RegisterForm />
       </div>
     </div>
   )
--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -78,7 +78,7 @@ const Navbar = ({
   ],
   auth = {
     login: { title: "Login", url: "/login" },
-    signup: { title: "Sign up", url: "/signup" },
+    signup: { title: "Register", url: "/register" },
   },
   className,
 }: Navbar1Props) => {

99b515bc4e2fd8d6379ccc93d2d342a5740c7549
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-01-18 22:41:36 +0800

    get cookies

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,8 +1,16 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
 export default async function Home() {
-  const session= await authClient.getSession();
+  const cookieStore=await cookies();
+  console.log(cookieStore.toString());
+  const res=await fetch("http://localhost:4000/api/auth/get-session",{
+    headers:{
+      Cookie: cookieStore.toString(),
+    },
+    cache:"no-store"
+  })
+  const session=await res.json();
   console.log(session)
   return (
     <div>

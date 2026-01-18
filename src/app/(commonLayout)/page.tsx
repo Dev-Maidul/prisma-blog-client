0643b339d171f49a86146916dd458313566cc727
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-01-19 07:11:51 +0800

    show blog post on home page

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,12 +1,16 @@
-import { Button } from "@/components/ui/button";
+
+import BlogCard from "@/components/homepage/BlogCard";
 import { blogService } from "@/services/blog.service";
+import { BlogPost } from "@/types";
 
 export default async function Home() {
   const {data}=await blogService.getBlogPosts();
-  console.log(data);
+  // console.log(data);
   return (
-    <div>
-      <Button variant={"outline"}>Click here</Button>
+    <div className="grid grid-cols-4 gap-4 px-4 w-10/12 mx-auto">
+      {data?.data?.map((post: BlogPost) => (
+        <BlogCard key={post.id} post={post} />
+      ))}
     </div>
   );
 }
